05ce6b94f726be1ce4b47d31eb7270eec09347c5
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-09-01 09:57:35 +0600

    update

--- js/ptube.js
+++ js/ptube.js
@@ -1,3 +1,4 @@
+// loading api data
 const loadcontant = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
@@ -7,7 +8,7 @@ const loadcontant = async () => {
 }
 // show category
 const showcategory = (data) => {
-    console.log(data)
+    // console.log(data)
     const catagorySection = document.getElementById('catagory-button')
     data.forEach(catData => {
         const div = document.createElement('div');
@@ -20,7 +21,7 @@ const showcategory = (data) => {
         `
 
 
-        console.log(catData.category)
+        // console.log(catData.category)
         catagorySection.appendChild(div)
     });
 
@@ -28,51 +29,83 @@ const showcategory = (data) => {
 
 // category contant
 const catagory = async (id) => {
-    console.log("this is all", id)
+    // console.log("this is all", id)
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json()
     let video_details = data.data;
     console.log(video_details)
 
-   card(video_details);
-    
+    card(video_details);
+
 }
 
 //category card
 
-const card=(video_details)=>{
+const card = (video_details) => {
+    // get parent element by id
     const phTube_video_card = document.getElementById('phTube-video');
     phTube_video_card.textContent = '';
 
-    video_details.forEach(element => {
-        console.log(element)
-        // console.log(element.authors.profile_picture)
-// if (element !="") {
-     // phTube-video card
-     const div = document.createElement('div');
-     div.classList = "card card-compact bg-base-100 shadow-xl mt-10";
-     div.innerHTML = `
- <figure><img src="${element.thumbnail}" alt="Shoes" /></figure>
- <div class="card-body flex flex-row">
-   <div class="w-3/12">
-   <img src=${element.authors[0].profile_picture}" id="authors-img"/>
-   </div>
-   <div class="">
-   <h2 class="card-title text-lg">${element.title}</h2>
-  
-   <p class='flex items-center gap-2'>
-${element.authors[0].profile_name}
-${element.authors[0].verified ? '<img src="./image/verified-icon.webp"  class="w-[40px]"/>' : ''}
-</p>
-   <p>${element.others.views}</p>
-   </div>
- </div>
- `
-// }
-       
-        phTube_video_card.appendChild(div)
-    });
+    // console.log(video_details.length);
+
+    // phtube card which is not empty
+    if (video_details.length !== 0) {
+        video_details.forEach(element => {
+            //convart time
+            const { hour, min } = convartTime(element.others.posted_date)
+            console.log(`${hour} hour: ${min}min`)
+            console.log(element)
+            // console.log(element.authors.profile_picture)
+            // if (element !="") {
+            // phTube-video card
+            const div = document.createElement('div');
+            div.classList = "card card-compact bg-base-100 shadow-xl mt-10";
+            div.innerHTML = `
+     <figure><img src="${element.thumbnail}" alt="Shoes" class="h-[200px]" /></figure>
+     <p>20 hour 10 min<p>
+     <div class="card-body flex flex-row">
+       <div class="w-[20%]">
+       <img src=${element.authors[0].profile_picture}" id="authors-img"/>
+       </div>
+       <div class="">
+       <h2 class="card-title text-lg">${element.title}</h2>
+      
+       <p class='flex items-center gap-2'>
+    ${element.authors[0].profile_name}
+    ${element.authors[0].verified ? '<img src="./image/verified-icon.png"  class="w-[20px]"/>' : ''}
+    </p>
+       <p>${element.others.views}</p>
+       </div>
+     </div>
+     `
+            // }
+
+            phTube_video_card.appendChild(div)
+        });
+    }
+    // phtube card which is empty
+
+    else {
+        const div = document.createElement('div');
+        div.classList = "text-center mx-auto w-[100%] md:w-[200%] lg:w-[400%] mt-20"
+        div.innerHTML = `
+        <img src="./image/icon.png" class="mx-auto"/>
+           <h1 class="text-4xl font-bold">Oops!! Sorry, There is no </br>content here</h1>
+        `
+        phTube_video_card.appendChild(div);
+    }
+
 
 }
+//convart seconds to hour and minuts
 
+const convartTime = (seconds) => {
+    const hour = Math.floor(seconds / 3600);
+    const min = (Math.floor(seconds%3600)/ 60).toFixed(2);
+    // min.toFixed(2)
+ 
+    return {hour,min}
+
+
+}
 loadcontant()
